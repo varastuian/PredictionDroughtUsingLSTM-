477ad24ef1a954efc09a8018bcfbf1f1fa879c91
vahidHome <varastuian@gmail.com>
2025-03-31 12:11:16 +0330

    UPDATE

--- Codes/lstm22.py
+++ Codes/lstm22.py
@@ -5,11 +5,22 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
-import os
+from sklearn.metrics import mean_squared_error
+import pywt
 
 df = pd.read_csv('result/40708spi.txt', delimiter=' ')
-timeseries = df[['spi1']].values.astype('float32')
+# timeseries = df[['spi1']].values.astype('float32')
+timeseries = df['spi1'].values.astype('float32')
 
+wavelet = 'db4'
+level = 2
+coeffs = pywt.wavedec(timeseries, wavelet, level=level)
+# Approximation: a denoised/smoothed version
+approx = pywt.upcoef('a', coeffs[0], wavelet, level=level, take=len(timeseries))
+# Detail from level 1 (used for boosted methods)
+detail = pywt.upcoef('d', coeffs[1], wavelet, level=level, take=len(timeseries))
+timeseries_org = timeseries
+timeseries = approx
 # train-test split for time series
 train_size = int(len(timeseries) * 0.67)
 # test_size = len(timeseries) - train_size
@@ -23,12 +34,18 @@ def create_dataset(dataset, lookback):
         target = dataset[i+1:i+lookback+1]
         X.append(feature)
         y.append(target)
-    return torch.tensor(X), torch.tensor(y)
+    return np.array(X), np.array(y)
 
-lookback = 24
+lookback = 4
 X_train, y_train = create_dataset(train, lookback=lookback)
 X_test, y_test = create_dataset(test, lookback=lookback)
+X_train_lstm = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+X_test_lstm = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
 
+X_train_tensor = torch.tensor(X_train_lstm, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train.reshape(-1,1), dtype=torch.float32)
+X_test_tensor = torch.tensor(X_test_lstm, dtype=torch.float32)
+y_test_tensor = torch.tensor(y_test.reshape(-1,1), dtype=torch.float32)
 class lstmModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -42,16 +59,24 @@ class lstmModel(nn.Module):
 model = lstmModel()
 optimizer = optim.Adam(model.parameters())
 loss_fn = nn.MSELoss()
-loader = data.DataLoader(data.TensorDataset(X_train, y_train), shuffle=True, batch_size=8)
+
+
+# loader = data.DataLoader(data.TensorDataset(X_train, y_train), shuffle=True, batch_size=8)
+
+
+train_dataset = data.TensorDataset(X_train_tensor, y_train_tensor)
+batch_size = 8
+loader = data.DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
 
 checkpoint_path = 'lstm_model_checkpoint22.pth'
-if os.path.exists(checkpoint_path):
+# if os.path.exists(checkpoint_path):
+if False:
     model.load_state_dict(torch.load(checkpoint_path))
     print("Loaded pre-trained model.")
 else:
     print("Training model...")
 
-    n_epochs = 800
+    n_epochs = 300
     for epoch in range(n_epochs):
         model.train()
         for X_batch, y_batch in loader:
@@ -62,7 +87,7 @@ else:
             optimizer.step()
 
         # Validation
-        if epoch % 100 != 0:
+        if epoch % 50 != 0:
             continue
         model.eval()
         with torch.no_grad():
@@ -70,33 +95,28 @@ else:
             train_rmse = np.sqrt(loss_fn(y_pred, y_train))
             y_pred = model(X_test)
             test_rmse = np.sqrt(loss_fn(y_pred, y_test))
+
         print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
-    torch.save(model.state_dict(), checkpoint_path)
-    print("Model trained and saved.")
+    # torch.save(model.state_dict(), checkpoint_path)
+    # print("Model trained and saved.")
 
 
 with torch.no_grad():
     # shift train predictions for plotting
-    # train_plot = np.ones_like(timeseries) * np.nan
-    # y_pred = model(X_train)
-    # y_pred = y_pred[:, -1, :]
-    # train_plot[lookback:train_size] = model(X_train)[:, -1, :]
+    train_plot = np.ones_like(timeseries) * np.nan
+    y_pred = model(X_train)
+    y_pred = y_pred[:, -1, :]
+    train_plot[lookback:train_size] = model(X_train)[:, -1, :]
     # shift test predictions for plotting
-    # test_plot = np.ones_like(timeseries) * np.nan
-    test_plot = model(X_test)[:, -1, :]
-
+    test_plot = np.ones_like(timeseries) * np.nan
+    test_plot[train_size+lookback:len(timeseries)] = model(X_test)[:, -1, :]
 # plot
-plt.plot(timeseries[train_size+lookback:len(timeseries)], marker='o')
-# plt.plot(train_plot, c='r')
-plt.plot(test_plot, c='g', marker='x', linestyle='--')
+plt.figure(figsize=(14,12))
 
-
-# forecast_dates = pd.date_range(start=timeseries.index[-1] + pd.DateOffset(months=1), periods=24, freq='MS')
-# forecast_df = pd.DataFrame({'Date': forecast_dates, 'Predicted_SPI': test_plot.flatten()})
-# print("24-Month Forecast for SPI:")
-# print(forecast_df)
-
-# plt.plot(forecast_df['Date'], forecast_df['Predicted_SPI'], marker='o', label='Forecast')
+plt.plot(timeseries_org,label='actual spi',marker='o')
+plt.plot(train_plot,label='trained spi', c='r',marker='o')
+plt.plot(test_plot, label='test spi',c='g',marker='o')
+plt.legend()
 
 plt.show()
 
